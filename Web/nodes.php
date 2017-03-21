a382621b8a6319aea684ddfa13b20cd4d98e5bc7
adrianritchie <ar@kodo.gg>
2017-03-21 12:44:02 +0000

    Create nodes.php
    Temp data file for storing machine nodes.

---
+++ Web/nodes.php
@@ -0,0 +1,9 @@
+<?php
+$nodes = [
+        [
+            'mac_addr' => '60:01:94:17:8E:6B',
+            'name' => 'Mill 1 Men\'s Shed',
+            'access' => SAFETY_MACHINING | TOOL_MILL
+            // You need to have done the Machining Safety and have mill-access
+        ],
+    ]
